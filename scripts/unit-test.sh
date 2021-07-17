64940d40a0eff845fe3481d6d9ebe13fa2513a0e
Joshua Swick <31593215+joshuaswickirl@users.noreply.github.com>
2021-07-17 11:49:52 -0600

    Add integers pkg and color failed tests (#9)

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go test -coverprofile=coverage.out ./...
+go test -coverprofile=coverage.out ./... | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
 go tool cover -func=coverage.out
