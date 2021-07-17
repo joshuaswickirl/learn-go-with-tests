43b7b04907ac7b07dfe9325d27424162f28d0614
Joshua Swick <joshuaswickirl@gmail.com>
2021-07-17 08:18:21 -0600

    Add name arg to hello

--- internal/hello/hello_test.go
+++ internal/hello/hello_test.go
@@ -7,10 +7,25 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := hello.Hello("Josh")
-	want := "Hello, Josh"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello.Hello("Josh")
+		want := "Hello, Josh"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := hello.Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
