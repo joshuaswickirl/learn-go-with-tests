1ad8fad9a56349ad189125a23a1497d37b5ad90d
Joshua Swick <joshuaswickirl@gmail.com>
2021-07-20 08:15:17 -0600

    Separate unit-test and code coverage into different steps

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-go test -bench=. -coverprofile=coverage.out ./... | \
+go test -bench=. -v -coverprofile=coverage.out ./... | \
     sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | \
     sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
-echo
-go tool cover -func=coverage.out
