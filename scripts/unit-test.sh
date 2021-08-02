12523490f475ca462e4aa6046500cc5be539d207
Joshua Swick <31593215+joshuaswickirl@users.noreply.github.com>
2021-08-02 11:09:03 -0600

    Add concurrency, bench script, and test for race conditions in unit tests (#17)

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-go test -coverprofile=coverage.out ./... | \
+go test -race -coverprofile=coverage.out ./... | \
     sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | \
     sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
