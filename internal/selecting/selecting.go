568a2cdecfe8d92fcc3a87dbd156ffe44319cc87
Joshua Swick <joshuaswickirl@gmail.com>
2021-08-02 13:57:04 -0600

    Add select

---
+++ internal/selecting/selecting.go
@@ -0,0 +1,33 @@
+package selecting
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func WebsiteRacer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-makeHTTPRequest(a):
+		return a, nil
+	case <-makeHTTPRequest(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func makeHTTPRequest(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		ch <- struct{}{}
+	}()
+	return ch
+}
