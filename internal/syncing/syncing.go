5d5836d4c57b316037c76aeee5b34f05e46d88de
Joshua Swick <joshuaswickirl@gmail.com>
2021-08-02 18:48:18 -0600

    Add sync

---
+++ internal/syncing/syncing.go
@@ -0,0 +1,22 @@
+package syncing
+
+import "sync"
+
+type Counter struct {
+	m     sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.m.Lock()
+	defer c.m.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
