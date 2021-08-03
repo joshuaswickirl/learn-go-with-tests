2cf938f735814591a13608585e74db7a006026d6
Joshua Swick <31593215+joshuaswickirl@users.noreply.github.com>
2021-08-02 18:50:51 -0600

    Add sync (#21)

---
+++ internal/syncing/syncing_test.go
@@ -0,0 +1,46 @@
+package syncing_test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/joshuaswickirl/learn-go-with-tests/internal/syncing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := syncing.Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		if counter.Value() != 3 {
+			t.Errorf("got %d, want %d", counter.Value(), 3)
+		}
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := syncing.NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *syncing.Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
